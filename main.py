83d29ed15a6e1bcd140f24f1028e4614d465b88f
eadietz <emmanuelle.dietz@posteo.de>
2026-07-08 15:26:41 +0200

    added readme, some translations

--- main.py
+++ main.py
@@ -41,15 +41,15 @@ analyzedModelIndex = 0
 all_data_frames = {}
 costs = []
 modelAtomTemplates = [    
-        {'name':'movePerson','filter':lambda s: s.name=='movePerson', 'columns':['name']},
+    {'name':'movePerson','filter':lambda s: s.name=='movePerson', 'columns':['name']},
     {'name':'currentRoom','filter':lambda s: s.name=='currentRoom', 'columns':['name','room']},
-    {'name':'allokation', 'filter':lambda s: s.name=='allokation', 'columns':['klasse','raum','fach', 'tag', 'stunde','lehrer']},
+    {'name':'allocation', 'filter':lambda s: s.name=='allocation', 'columns':['class','room','subject', 'day', 'timeslot','teacher']},
     {'name':'sameRoomSameProject', 'filter':lambda s: s.name=='sameRoomSameProject', 'columns':['name1','name2']},
     {'name':'dist', 'filter':lambda s: s.name=='dist', 'columns':['start','ende','dist']},
-    {'name':'wegkosten', 'filter':lambda s: s.name== 'wegkosten', 'columns':['klasse', 'dist','tag', 'stunde']},
+    {'name':'total_distance', 'filter':lambda s: s.name== 'total_distance', 'columns':['class', 'dist','day', 'timeslot']},
     {'name':'invalid', 'filter':lambda s: s.name=='invalid', 'columns':['name']},
-    {'name':'wegkostenlehrer', 'filter':lambda s: s.name== 'wegkostenlehrer', 'columns':['lehrer', 'dist','tag', 'stunde']},
-    {'name':'sumblock', 'filter':lambda s: s.name=='sumblock', 'columns':['anzahl']}
+    {'name':'travel_cost_teacher', 'filter':lambda s: s.name== 'travel_cost_teacher', 'columns':['teacher', 'dist','day', 'timeslot']},
+    {'name':'sumblock', 'filter':lambda s: s.name=='sumblock', 'columns':['nr']}
 
 ]
 
